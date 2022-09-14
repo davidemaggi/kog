a9b694803b3b5058e2e5f8629f528a13d8a868a9
Davide <davide.maggi@live.com>
2022-09-14 15:36:21 +0200

    Added Choco release

--- deploy/chocolatey/tools/chocolateyinstall.ps1
+++ deploy/chocolatey/tools/chocolateyinstall.ps1
@@ -3,14 +3,19 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'kog'
 $version = 'v0.0.1-alpha-02'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64        = "https://github.com/davidemaggi/kog/releases/download/$version/kog-$version-windows-386.zip"
 $url        = "https://github.com/davidemaggi/kog/releases/download/$version/kog-$version-windows-amd64.zip"
-$hash = '025f9b74cbcc876c7e9ce0b22121f13e'
+$hash = 'ebd1b3cdf4df932b66f5fe924c6978a0'
+$hash64 = '025f9b74cbcc876c7e9ce0b22121f13e'
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
+  url64bit      = $url64
   url           = $url
   checksum      = $hash
+  checksum64    = $hash64
   checksumType  = 'MD5'
+  checksumType64  = 'MD5'
 }
 
 Install-ChocolateyZipPackage @packageArgs
