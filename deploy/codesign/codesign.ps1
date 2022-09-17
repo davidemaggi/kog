45c7078c133a09e8cdf97738d777a635cd01c688
Davide <davide.maggi@live.com>
2022-09-17 14:32:59 +0200

    Fixed source url

--- deploy/codesign/codesign.ps1
+++ deploy/codesign/codesign.ps1
@@ -1,6 +1,5 @@
-Write-Host $Env:GITHUB_REF
-Write-Host $Env:GITHUB_ENV
-Write-Host $Env.CHOCO_TOKEN
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Write-Output $Env:GITHUB_REF
+Write-Output $Env:GITHUB_ENV
+Write-Output $Env.CHOCO_TOKEN
 
 choco install windows-sdk-10-version-2104-all -y
