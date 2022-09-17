7645164c3caf9d7eaf55a783571e6a9a0510db1c
Davide <davide.maggi@live.com>
2022-09-17 14:23:14 +0200

    Fixed source url

--- deploy/codesign/codesign.ps1
+++ deploy/codesign/codesign.ps1
@@ -1,3 +1,4 @@
 Write-Host $Env:GITHUB_REF
 Write-Host $Env:GITHUB_ENV
 Write-Host $Env.CHOCO_TOKEN
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
